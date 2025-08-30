aae9e676f36adc3bf313da8b648eba5899e551c8
Jobet P. Casquejo <jcasquejo56@gmail.com>
2025-08-31 07:09:22 +0800

    first commit

---
+++ src/main/java/com/edutrack/edutrack/config/DatabaseConfig.java
@@ -0,0 +1,35 @@
+package com.edutrack.edutrack.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        dataSourceBuilder.driverClassName(driverClassName);
+        return dataSourceBuilder.build();
+    }
+}
